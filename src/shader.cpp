2aee18733a10abec3ac0e8bbad5ed24f4b6c54ba
Tapio Vierros <tapiovierros@gmail.com>
2014-01-28 23:38:44 +0200

    Add shader error checking and log printing.

--- src/shader.cpp
+++ src/shader.cpp
@@ -29,27 +29,62 @@ ShaderProgram::~ShaderProgram()
 
 bool ShaderProgram::compile(ShaderType type, const string& text)
 {
-	if (text.empty()) return false;
 	GLsizei lengths[] = { (GLsizei)text.length() };
 	const GLchar* strings[] = { (const GLchar*)text.c_str() };
 	uint& shaderId = shaderIds[type];
 	shaderId = glCreateShader(convertToGL(type));
-	if (!shaderId) return false;
 	glShaderSource(shaderId, 1, strings, lengths);
 	glCompileShader(shaderId);
+
+	// Get shader log
+	string infoLog;
+	GLint infoLogLength;
+	glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if (infoLogLength > 1) {
+		infoLog.resize(infoLogLength);
+		glGetShaderInfoLog(shaderId, infoLogLength, NULL, &infoLog[0]);
+	}
+	// Check compilation status
+	GLint status;
+	glGetShaderiv(shaderId, GL_COMPILE_STATUS, &status);
+	if (!status) {
+		logError("Failed to compile shader: %s", infoLog.c_str());
+		return false;
+	}
+	if (!infoLog.empty()) {
+		logWarning("Warning(s) compiling shader:\n%s", infoLog.c_str());
+	}
+
 	return true;
 }
 
 bool ShaderProgram::link()
 {
 	id = glCreateProgram();
-	if (!id) return false;
-
 	for (auto i : shaderIds) {
 		if (i > 0) glAttachShader(id, i);
 	}
-
 	glLinkProgram(id);
+
+	// Get program log
+	string infoLog;
+	GLint infoLogLength;
+	glGetProgramiv(id, GL_INFO_LOG_LENGTH, &infoLogLength);
+	if (infoLogLength > 1) {
+		infoLog.resize(infoLogLength);
+		glGetProgramInfoLog(id, infoLogLength, NULL, &infoLog[0]);
+	}
+	// Check linking status
+	int status;
+	glGetProgramiv(id, GL_LINK_STATUS, &status);
+	if (!status) {
+		logError("Failed to link program:\n%s", infoLog.c_str());
+		return false;
+	}
+	if (!infoLog.empty()) {
+		logWarning("Warning(s) linking program:\n%s", infoLog.c_str());
+	}
+
 	return true;
 }
 
