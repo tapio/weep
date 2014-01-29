f8278c72aaefa35284f0d6766984ac0b19aa8dcf
Tapio Vierros <tapiovierros@gmail.com>
2014-01-29 20:48:43 +0200

    Fix crash on exit.

--- game/main.cpp
+++ game/main.cpp
@@ -9,19 +9,17 @@ int main(int argc, char* argv[])
 {
 	Platform::init();
 
-	Geometry geom = Geometry::createPlane(100, 100);
-
 	ShaderProgram shader;
 	shader.compile(VERTEX_SHADER, readFile("shaders/core.vert"));
 	shader.compile(FRAGMENT_SHADER, readFile("shaders/core.frag"));
 	shader.link();
 
-	Material mat;
-	mat.shaderId = shader.id;
-
 	Model model;
-	model.geometry = std::shared_ptr<Geometry>(&geom);
-	model.material = std::shared_ptr<Material>(&mat);
+	model.geometry = std::shared_ptr<Geometry>(new Geometry());
+	*model.geometry = Geometry::createPlane(100, 100);
+	model.material = std::shared_ptr<Material>(new Material());
+	model.material->shaderId = shader.id;
+
 	GetRenderer().addModel(&model);
 
 	Platform::run();
