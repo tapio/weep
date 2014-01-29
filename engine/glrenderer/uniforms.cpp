97abbc7e87890665c97376346fd542df1dbfac00
Tapio Vierros <tapiovierros@gmail.com>
2014-01-29 23:10:01 +0200

    Add classes for uniform blocks.

---
+++ engine/glrenderer/uniforms.cpp
@@ -0,0 +1,30 @@
+#include "uniforms.hpp"
+#include "glutil.hpp"
+
+template<typename T>
+void UBO<T>::create(uint binding)
+{
+	glGenBuffers(1, &id);
+	glBindBuffer(GL_UNIFORM_BUFFER, id);
+	glBindBufferBase(GL_UNIFORM_BUFFER, binding, id);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(uniforms), (const GLvoid*)&uniforms, GL_DYNAMIC_DRAW);
+}
+
+template<typename T>
+void UBO<T>::upload()
+{
+	glBindBuffer(GL_UNIFORM_BUFFER, id);
+	glBufferData(GL_UNIFORM_BUFFER, sizeof(uniforms), (const GLvoid*)&uniforms, GL_DYNAMIC_DRAW);
+}
+
+template<typename T>
+void UBO<T>::destroy()
+{
+	if (id) {
+		glDeleteBuffers(1, &id);
+		id = 0;
+	}
+}
+
+template class UBO<UniformCommonBlock>;
+template class UBO<UniformColorBlock>;
---
+++ engine/glrenderer/uniforms.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include "common.hpp"
+
+struct UniformCommonBlock {
+	mat4 modelMatrix = mat4();
+	mat4 modelViewMatrix = mat4();
+	mat4 projectionMatrix = mat4();
+	mat4 viewMatrix = mat4();
+	mat3 normalMatrix = mat3();
+	vec3 cameraPosition = vec3();
+};
+
+struct UniformColorBlock {
+	vec3 ambient = vec3();
+	vec3 diffuse = vec3();
+	vec3 specular = vec3();
+};
+
+template<typename T>
+class UBO
+{
+public:
+	NONCOPYABLE(UBO);
+
+	uint id = 0;
+	int binding = 0;
+	T uniforms = T();
+
+	UBO() {}
+
+	void create(uint binding);
+	void upload();
+	void destroy();
+};
