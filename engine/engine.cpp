5c5b978e9604cc62156f7180f6fe0a24df0e856c
Tapio Vierros <tapiovierros@gmail.com>
2015-08-27 01:21:45 +0300

    Set some gl attributes.

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -35,8 +35,13 @@ void Engine::init()
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	int contextFlags = SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG;
 	if (settings["renderer"]["gldebug"].bool_value())
-		SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG);
+		contextFlags |= SDL_GL_CONTEXT_DEBUG_FLAG;
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, contextFlags);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
 	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 	int msaa = settings["renderer"]["msaa"].int_value();
