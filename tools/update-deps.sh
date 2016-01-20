cd6477a603b56349c48ee0629d62119aa3d5e685
Tapio Vierros <tapiovierros@gmail.com>
2016-01-20 21:41:40 +0200

    Download in the background in update-deps script.

--- tools/update-deps.sh
+++ tools/update-deps.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 ROOT="$(dirname "$(readlink -f "$0")")"/..
 GHBASEURL=https://raw.githubusercontent.com
@@ -7,25 +7,26 @@ WGET="wget -nv"
 cd "$ROOT/third-party"
 
 # IQM
-$WGET $GHBASEURL/lsalzman/iqm/master/iqm.h -O iqm/iqm.h
+$WGET $GHBASEURL/lsalzman/iqm/master/iqm.h -O iqm/iqm.h &
 
 # Json11
-$WGET $GHBASEURL/dropbox/json11/master/json11.hpp -O json11/json11.hpp
-$WGET $GHBASEURL/dropbox/json11/master/json11.cpp -O json11/json11.cpp
-$WGET $GHBASEURL/dropbox/json11/master/LICENSE.txt -O json11/LICENSE.txt
+$WGET $GHBASEURL/dropbox/json11/master/json11.hpp -O json11/json11.hpp &
+$WGET $GHBASEURL/dropbox/json11/master/json11.cpp -O json11/json11.cpp &
+$WGET $GHBASEURL/dropbox/json11/master/LICENSE.txt -O json11/LICENSE.txt &
 
 # Remotery
-$WGET $GHBASEURL/Celtoys/Remotery/master/lib/Remotery.h -O remotery/Remotery.h
-$WGET $GHBASEURL/Celtoys/Remotery/master/lib/Remotery.c -O remotery/Remotery.c
-$WGET $GHBASEURL/Celtoys/Remotery/master/LICENSE -O remotery/LICENSE
+$WGET $GHBASEURL/Celtoys/Remotery/master/lib/Remotery.h -O remotery/Remotery.h &
+$WGET $GHBASEURL/Celtoys/Remotery/master/lib/Remotery.c -O remotery/Remotery.c &
+$WGET $GHBASEURL/Celtoys/Remotery/master/LICENSE -O remotery/LICENSE &
 
 # stb_image
-$WGET $GHBASEURL/nothings/stb/master/stb_image.h -O stb_image/stb_image.h
+$WGET $GHBASEURL/nothings/stb/master/stb_image.h -O stb_image/stb_image.h &
 
 # ImGui
 for f in imconfig.h imgui.cpp imgui.h imgui_demo.cpp imgui_draw.cpp imgui_internal.h LICENSE stb_rect_pack.h stb_textedit.h stb_truetype.h; do
-	$WGET $GHBASEURL/ocornut/imgui/master/$f -O imgui/$f
+	$WGET $GHBASEURL/ocornut/imgui/master/$f -O imgui/$f &
 done
-$WGET $GHBASEURL/ocornut/imgui/master/examples/sdl_opengl3_example/imgui_impl_sdl_gl3.h -O imgui/imgui_impl_sdl_gl3.h
-$WGET $GHBASEURL/ocornut/imgui/master/examples/sdl_opengl3_example/imgui_impl_sdl_gl3.cpp -O imgui/imgui_impl_sdl_gl3.cpp
+$WGET $GHBASEURL/ocornut/imgui/master/examples/sdl_opengl3_example/imgui_impl_sdl_gl3.h -O imgui/imgui_impl_sdl_gl3.h &
+$WGET $GHBASEURL/ocornut/imgui/master/examples/sdl_opengl3_example/imgui_impl_sdl_gl3.cpp -O imgui/imgui_impl_sdl_gl3.cpp &
+
 
