61e6a37b5ed9dff75401d28d30bc1b581cdba202
Tapio Vierros <tapiovierros@gmail.com>
2017-10-29 18:37:40 +0200

    Fix typo.

--- engine/glrenderer/gles_stub.c
+++ engine/glrenderer/gles_stub.c
@@ -16,6 +16,6 @@ void APIENTRY glTexImage2DMultisample (GLenum target, GLsizei samples, GLenum in
 
 void APIENTRY glFramebufferTexture (GLenum target, GLenum attachment, GLuint texture, GLint level)
 {
-	printf("GLES STUB: glTexImage2DMultisample\n");
+	printf("GLES STUB: glFramebufferTexture\n");
 	glFramebufferTexture2D(target, attachment, GL_TEXTURE_2D, texture, level);
 }
