79a24c865b50d05e109d1a7646ca8f411e803d85
Tapio Vierros <tapiovierros@gmail.com>
2021-11-26 09:38:49 +0200

    Allow local wget exe.

--- tools/update-deps/config.sh
+++ tools/update-deps/config.sh
@@ -1,7 +1,12 @@
 ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/../..
 PATCHDIR="$ROOT/tools/update-deps/patches"
 GHBASEURL=https://raw.githubusercontent.com
-WGET="wget -nv"
+
+if [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+	WGET="$ROOT/tools/wget.exe -nv"
+else
+	WGET="wget -nv"
+fi
 
 cd "$ROOT/third-party"
 
