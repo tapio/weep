f6fafe8991d5e6009d2aa6940791ccb45b9ae14e
Tapio Vierros <tapiovierros@gmail.com>
2021-12-08 16:25:52 +0200

    Untested SSBO struct based on UBO.

--- engine/glrenderer/uniforms.cpp
+++ engine/glrenderer/uniforms.cpp
@@ -32,6 +32,40 @@ void UBO<T>::destroy()
 	}
 }
 
+
+
+template<typename T>
+SSBO<T>::~SSBO()
+{
+	destroy();
+}
+
+template<typename T>
+void SSBO<T>::create()
+{
+	glGenBuffers(1, &id);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, id);
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, binding, id);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(T) * buffer.size(), (const GLvoid*)buffer.data(), GL_DYNAMIC_DRAW);
+}
+
+template<typename T>
+void SSBO<T>::upload()
+{
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, id);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(T) * buffer.size(), (const GLvoid*)buffer.data(), GL_DYNAMIC_DRAW);
+}
+
+template<typename T>
+void SSBO<T>::destroy()
+{
+	if (id) {
+		glDeleteBuffers(1, &id);
+		id = 0;
+	}
+}
+
+
 template struct UBO<UniformCommonBlock>;
 template struct UBO<UniformObjectBlock>;
 template struct UBO<UniformMaterialBlock>;
@@ -39,3 +73,10 @@ template struct UBO<UniformLightBlock>;
 template struct UBO<UniformCubeMatrixBlock>;
 template struct UBO<UniformSkinningBlock>;
 template struct UBO<UniformPostProcessBlock>;
+
+template struct SSBO<float>;
+template struct SSBO<int>;
+template struct SSBO<uint>;
+template struct SSBO<vec2>;
+template struct SSBO<vec3>;
+template struct SSBO<vec4>;
--- engine/glrenderer/uniforms.hpp
+++ engine/glrenderer/uniforms.hpp
@@ -2,6 +2,8 @@
 #include "common.hpp"
 #include "../../data/shaders/uniforms.glsl"
 
+
+// Uniform Buffer Object
 template<typename T>
 struct UBO
 {
@@ -16,4 +18,23 @@ struct UBO
 
 	uint id = 0;
 	T uniforms = T();
+};
+
+
+// Shader Storage Buffer Object
+template<typename T>
+struct SSBO
+{
+	SSBO(uint binding): binding(binding) {}
+	~SSBO();
+
+	NONCOPYABLE(SSBO);
+
+	void create();
+	void upload();
+	void destroy();
+
+	uint id = 0;
+	uint binding = 0;
+	std::vector<T> buffer;
 };
